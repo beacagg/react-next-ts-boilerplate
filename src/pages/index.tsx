6f5adae8d2b87d67972e43c62c09e17edc7ec8db
Beatriz Caggiano <beatrizcaggianob@gmail.com>
2020-11-08 18:40:02 -0300

    add support to husky and lint-staged

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+type Props = {
+  title: string
+}
+
+const foo = 'blabla'
+
+export default function Hom    e({ title = 'React' }: Props) {
+  return (
+    <>
+      oiiiii
+      {title}
+    </>
+  )
+}
